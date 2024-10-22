dd3c0ae1391b39ea244e8cc1ed8ab39d96ece1d4
Lukas <meink2220@gmail.com>
2024-10-22 17:37:48 +0200

    Funktioniert

--- src/Projectile.java
+++ src/Projectile.java
@@ -26,23 +26,17 @@ public class Projectile extends Entity {
      * Bewegung des Targets pro Timer Schritt
      */
     void Move(){
-//        System.out.println("Name: " + target.card.name + " X: " + target.cords[0] + " Y: " + target.cords[1]);
-        //tangens für winkel, speed ist hypo
-//        double a = Math.toDegrees(Math.tanh(DistanceInDirection(middleOfTarget)[1] / DistanceInDirection(target.cords)[0]));
-//        double xChange = Math.cos(a) * card.speed;
-//        double yChange = Math.sin(a) * card.speed;
+        //a-tangens(GK / AK) = winkel, speed ist hypo → Strahlensatz :)
         double[] distance = DistanceInDirection(middleOfTarget);
-        double relation = Math.abs(distance[0] / distance[1]);
-        //Änderung in x und y Richtung; Als Variable, weil wird sonst zu schnell
-        double xChange = (double) card.speed / 20 * relation * (distance[0] / Math.abs(distance[0]));
-        double yChange = (double) card.speed / 20 / relation * (distance[1] / Math.abs(distance[1]));
+        double a = Math.toDegrees(Math.atan(distance[1] / distance[0]));
+        double xChange = Math.cos(a) * card.speed / 10;
+        double yChange = Math.sin(a) * card.speed / 10;
         cords[0] += xChange;
         cords[1] += yChange;
     }
 
     void Update(ArrayList<Troop> troops, ArrayList<Tower> towers, ArrayList<Entity> bridges){
-        middleOfTarget = new double[]{target.cords[0] + (double) caster.card.width / 2, target.cords[1] + (double) caster.card.height / 2};
-        System.out.println("Target Cords: " + Arrays.toString(middleOfTarget));
+        middleOfTarget = new double[]{target.cords[0] + (double) target.card.width / 2, target.cords[1] + (double) target.card.height / 2};
         Move();
         label.setBounds((int) cords[0] - card.width / 2, (int) cords[1] - card.width / 2, card.width, card.height);
         TargetHit();
