f5eb4e6110615a2f0c97b569439246b805a0c879
Lukas <meink2220@gmail.com>
2024-10-24 17:01:55 +0200

    Funktioniert

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -124,7 +124,7 @@ public class Spielfeld {
                 switcher = 1;
             } else {
 //              normalerweise = 2
-                switcher = 2;
+                switcher = 1;
             }
             NewTroop(selectedTroop, x, y, players[switcher]);
         }catch (Exception e){
