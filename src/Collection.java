064a525394a2db994ef2483851be1672bf836bc0
Lukas <meink2220@gmail.com>
2024-10-25 00:17:12 +0200

    Funktioniert

--- src/Collection.java
+++ src/Collection.java
@@ -4,21 +4,30 @@ import java.util.ArrayList;
 
 public class Collection extends JPanel {
     ArrayList<Card> cards;
-    JPanel cardView;
+    JPanel cardView, team;
 
     Collection(ArrayList<Card> cards) {
         this.cards = cards;
-        setLayout(new GridLayout(1, 1));
+        setLayout(new GridLayout(2, 1));
+        CreateTeam(new Card[0]);
         PaintCards();
+
+        BackgroundColors();
         Add();
     }
 
     void Add(){
+        add(team);
         add(cardView);
     }
 
+    void BackgroundColors(){
+        cardView.setBackground(MainUI.vibe);
+//        team.setBackground(MainUI.vibe);
+    }
+
     void PaintCards() {
-        cardView = new JPanel(new GridLayout(10, 7));
+        cardView = new JPanel(new GridLayout(3, 7));
         for (Card card : cards) {
             if (!card.cardType.equals("troop") && !card.cardType.equals("tower")) continue;
             String[] shownCategories = new String[]{"Name:", "Speed:", "Range:", "Health:", "Damage:", "Attack Speed:", "Type:", "Elixir:"};
@@ -26,16 +35,26 @@ public class Collection extends JPanel {
         }
     }
     JPanel CreateCardInfoPanel(Card card, String[] categories) {
-        JPanel infoPanel = new JPanel(new GridLayout(1, 2));
+        JPanel infoPanel = new JPanel(new GridLayout(2, 1));
         infoPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         JPanel stats = new JPanel(new GridLayout(categories.length, 1));
         for (int i = 0; i < categories.length; i++) {
-            JLabel stat = new JLabel(categories[i] + " " + card.GetMultipleStats(categories).get(i));
+            JLabel stat = new JLabel(categories[i] + " " + card.GetMultipleStats(categories).get(i), SwingConstants.CENTER);
             stats.add(stat);
         }
         JLabel icon = new JLabel(card.icon);
         infoPanel.add(icon);
         infoPanel.add(stats);
         return infoPanel;
+    }
+
+    void CreateTeam(Card[] teamCards){
+        team = new JPanel(new GridLayout());
+        JPanel cardsOfTeam = new JPanel(new GridLayout(2, 4));
+        for (Card card : teamCards){
+            if (card == null ) continue;
+            cardsOfTeam.add(CreateCardInfoPanel(card, new String[]{""}));
+        }
+        team.add(cardsOfTeam);
     }
 }
